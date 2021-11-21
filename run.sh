b9462c9a6672ab52f2be461665092297614bc2ee
Yuriy Ilchenko <y.ilchenko@ytechteam.com>
2021-11-21 18:15:11 +0200

    upd run script

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ echo -n "Начало установки..."
 cp .env.example .env
 if docker-compose up -d --build; then
     docker-compose exec php-fpm composer install
+    docker-compose exec php-fpm php artisan key:generate
     docker-compose exec php-fpm php artisan migrate -n --force
     echo "\nГотово. В браузере набираем: http://localhost:8033"
 else
