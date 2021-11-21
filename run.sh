8eba84148d92aaea8efbdd31d0cd9e31d62670a2
Yuriy Ilchenko <y.ilchenko@ytechteam.com>
2021-11-21 18:07:13 +0200

    upd run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cp .env.example .env
 if docker-compose up -d --build; then
     docker-compose exec php-fpm composer install
     docker-compose exec php-fpm php artisan migrate -n --force
-    echo "\nГотово."
+    echo "\nГотово. В браузере набираем: http://localhost:8033"
 else
         echo "Не удалось запустить контейнеры!!!\n"
 fi
